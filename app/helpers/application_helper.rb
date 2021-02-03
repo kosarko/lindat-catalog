aacf6b1d1f648e53fb757f4c22fe913d98a2c8e0
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2021-02-03 21:24:52 +0100

    right/in copyright note
    linkify moved to application_helper
    
    linkify icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  include ActionView::Helpers::TagHelper
+  include Blacklight::IconHelperBehavior
+
+  def linkify(val)
+    return val unless val.start_with?('http')
+
+    tag.a(val, href: val) + blacklight_icon('fa_cc_by_4.0_international/external-link-alt-solid')
+
+  end
 end
--- app/presenters/blacklight/rendering/linkify.rb
+++ app/presenters/blacklight/rendering/linkify.rb
@@ -3,19 +3,11 @@
 module Blacklight
   module Rendering
     class Linkify < Blacklight::Rendering::AbstractStep
-      include ActionView::Helpers::TagHelper
+      include ApplicationHelper
       def render
         return next_step(values) unless config.linkify == true
 
         next_step(values.map { |x| linkify(x) })
-      end
-
-      private
-
-      def linkify(val)
-        return val unless val.start_with?('http')
-
-        tag.a val, href: val
       end
     end
   end
