d846c51587204e2269f8dae5b6cac0df170f9421
Ondřej Košarko <ko_ok@centrum.cz>
2021-02-08 11:21:46 +0100

    Sorting
    leaving relevance and display name to show it's configurable.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -237,10 +237,12 @@ class CatalogController < ApplicationController
     # whether the sort is ascending or descending (it must be asc or desc
     # except in the relevancy case). Add the sort: option to configure a
     # custom Blacklight url parameter value separate from the Solr sort fields.
-    config.add_sort_field 'relevance', sort: 'score desc, pub_date_si desc, title_si asc', label: 'relevance'
-    config.add_sort_field 'year-desc', sort: 'pub_date_si desc, title_si asc', label: 'year'
-    config.add_sort_field 'author', sort: 'author_si asc, title_si asc', label: 'author'
-    config.add_sort_field 'title_si asc, pub_date_si desc', label: 'title'
+    # config.add_sort_field 'relevance', sort: 'score desc, pub_date_si desc, title_si asc', label: 'relevance'
+    # config.add_sort_field 'year-desc', sort: 'pub_date_si desc, title_si asc', label: 'year'
+    # config.add_sort_field 'author', sort: 'author_si asc, title_si asc', label: 'author'
+    # config.add_sort_field 'title_si asc, pub_date_si desc', label: 'title'
+    config.add_sort_field 'relevance', sort: 'score desc, displayName_ssort asc', label: 'relevance'
+    config.add_sort_field 'displayName', sort: 'displayName_ssort asc', label: 'display name'
 
     # If there are more than this many search results, no spelling ("did you
     # mean") suggestion is offered.
