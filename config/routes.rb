c2052b15918f8347082792fc2cb46e1fd9020285
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2021-05-14 17:03:27 +0200

    loosen contstrains on id path param
    - it allows any char (especially .) except for /
    - tries to add the configured formats back (.xml works .dc_xml doesn't)
    - it's a block so it's not applied only to a single path
    
    Eventually will be rolled back, as we'll need to take care of IDs with /
    as well. It would be unnecessary complication.

--- config/routes.rb
+++ config/routes.rb
@@ -5,22 +5,25 @@ Rails.application.routes.draw do
   root to: "catalog#index"
   concern :searchable, Blacklight::Routes::Searchable.new
 
-  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
-    concerns :searchable
-    concerns :range_searchable
+  constraints(id: /[^\/]+(?=#{ ActionController::Renderers::RENDERERS.map{|e| "\\.#{e}\\z"}.join("|") })|[^\/]+/) do
+    resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
+      concerns :searchable
+      concerns :range_searchable
 
-  end
-  concern :exportable, Blacklight::Routes::Exportable.new
+    end
+    concern :exportable, Blacklight::Routes::Exportable.new
 
-  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
-    concerns :exportable
-  end
+    # SEE https://github.com/projectblacklight/blacklight/wiki/Configuring-rails-routes
+    resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+      concerns :exportable
+    end
 
-  resources :bookmarks do
-    concerns :exportable
+    resources :bookmarks do
+      concerns :exportable
 
-    collection do
-      delete 'clear'
+      collection do
+        delete 'clear'
+      end
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
