d85b22f212ec8017306f30af710e32883d166765
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2021-05-14 17:13:58 +0200

    Revert "loosen contstrains on id path param"
    This reverts commit c2052b15918f8347082792fc2cb46e1fd9020285.
    
    Unnecessary complication (see the comment of the reverted commit)

--- config/routes.rb
+++ config/routes.rb
@@ -5,25 +5,22 @@ Rails.application.routes.draw do
   root to: "catalog#index"
   concern :searchable, Blacklight::Routes::Searchable.new
 
-  constraints(id: /[^\/]+(?=#{ ActionController::Renderers::RENDERERS.map{|e| "\\.#{e}\\z"}.join("|") })|[^\/]+/) do
-    resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
-      concerns :searchable
-      concerns :range_searchable
+  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
+    concerns :searchable
+    concerns :range_searchable
 
-    end
-    concern :exportable, Blacklight::Routes::Exportable.new
+  end
+  concern :exportable, Blacklight::Routes::Exportable.new
 
-    # SEE https://github.com/projectblacklight/blacklight/wiki/Configuring-rails-routes
-    resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
-      concerns :exportable
-    end
+  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+    concerns :exportable
+  end
 
-    resources :bookmarks do
-      concerns :exportable
+  resources :bookmarks do
+    concerns :exportable
 
-      collection do
-        delete 'clear'
-      end
+    collection do
+      delete 'clear'
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
