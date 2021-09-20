5fb52312f8ce47bdbff86677ba7ffe3ecc244e08
Ondrej Kosarko <kosarko@ufal.mff.cuni.cz>
2021-09-20 16:51:04 +0200

    updated lindat-common
    For now not using the web components. We would need to include
    `lindat.js` with `<script type="module"` as it contains import
    statements. That is tricky because there's sprockets, or whatever,
    asset building pipeline.
    So plain header.htm/footer.htm and css include

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,6 @@
 //= require bootstrap
 //= require blacklight/blacklight
 
-// LINDAT
-//= require new_theme/public/js/lindat
-
-
-
 // For blacklight_range_limit built-in JS, if you don't want it you don't need
 // this:
 //= require 'blacklight_range_limit'
